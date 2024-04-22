bf5eb83f0a10f6298175f8cac672224d4751ab96
dzw <1756644571@qq.com>
2024-04-22 22:15:00 +0800

    feat: 新增个人设置功能

--- miniprogram/modules/settingModule/pages/address/add/index.js
+++ miniprogram/modules/settingModule/pages/address/add/index.js
@@ -1,10 +1,49 @@
 Page({
   // 页面的初始数据
-  data: {},
+  data: {
+    name: '', // 收货人
+    phone: '', // 手机号
+    provinceName: '', // 省
+    provinceCode: '', // 省 编码
+    cityName: '', // 市
+    cityCode: '', // 市 编码
+    districtName: '', // 区
+    districtCode: '', // 区 编码
+    address: '', // 详细地址
+    fullAddress: '', // 完整地址 (省 + 市 + 区 + 详细地址)
+    isDefault: false // 设置默认地址，是否默认地址 → 0：否  1：是
+  },
 
   // 保存收货地址
-  saveAddrssForm(event) {},
+  // 获取表单元素的值
+  saveAddrssForm(event) {
+    // 解构出省市区以及 是否是默认地址
+    const { provinceName, cityName, districtName, address, isDefault } = this.data
+
+    // 拼接完整的地址
+    const fullAddress = provinceName + cityName + districtName + address
+
+    // 合并接口请求参数
+    const params = {
+      ...this.data,
+      fullAddress,
+      isDefault: isDefault ? 1 : 0
+    }
+
+    console.log(params)
+  },
 
   // 省市区选择
-  onAddressChange(event) {}
+  onAddressChange(event) {
+    const [provinceName, cityName, districtName] = event.detail.value
+    const [provinceCode, cityCode, districtCode] = event.detail.code
+    this.setData({
+      provinceName,
+      cityName,
+      districtName,
+      provinceCode,
+      cityCode,
+      districtCode
+    })
+  }
 })
--- miniprogram/modules/settingModule/pages/profile/profile.js
+++ miniprogram/modules/settingModule/pages/profile/profile.js
@@ -1,14 +1,62 @@
 // pages/profile/profile.js
-
+import { userBehavior } from './behavior'
+import { getStorage, setStorage } from '../../../../utils/storage'
+import { reqUploadFile, reqUpdateUserInfo } from '../../../../api/user'
+import { toast } from '../../../../utils/extendApi'
 Page({
+  // 注册userBehavior
+  behaviors: [userBehavior],
+
   // 页面的初始数据
   data: {
     isShowPopup: false // 控制更新用户昵称的弹框显示与否
   },
 
+  getNickName(e) {
+    // console.log(e.detail.value.nickname)
+    this.setData({
+      'userInfo.nickname': e.detail.value.nickname,
+      isShowPopup: false
+    })
+  },
+
+  chooseAvatar(e) {
+    // wx.uploadFile({
+    //   filePath: e.detail.avatarUrl,
+    //   name: 'file',
+    //   url: 'https://gmall-prod.atguigu.cn/mall-api/fileUpload',
+    //   header: {
+    //     token: getStorage('token')
+    //   },
+    //   success: (res) => {
+    //     this.setData({
+    //       'userInfo.headimgurl': JSON.parse(res.data)?.data
+    //     })
+    //   }
+    // })
+    reqUploadFile(e.detail.avatarUrl, 'file').then((res) => {
+      this.setData({
+        'userInfo.headimgurl': res.data
+      })
+    })
+  },
+
+  updateUserInfo() {
+    reqUpdateUserInfo(this.data.userInfo).then((res) => {
+      if (res.code == 200) {
+        setStorage('userInfo', this.data.userInfo)
+        this.setUserInfo(this.data.userInfo)
+        toast({
+          title: '用户信息更新成功'
+        })
+      }
+    })
+  },
+
   // 显示修改昵称弹框
   onUpdateNickName() {
     this.setData({
+      'userInfo.nickname': this.data.userInfo.nickname,
       isShowPopup: true
     })
   },
